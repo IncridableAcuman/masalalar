7224288271e76cf8ee2c7ec8658cdf8ba5a911f0
Izzatbek <abdusharipovizzat03@gmail.com>
2025-09-02 22:02:10 +0500

    masala

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,23 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-void quickSort(vector<int> a){
-	int middle=a.size()/2;
+static void quickSort(int left,int right,int a[]){
+	int middle=(left+right)/2;
 	int x=a[middle];
-	int left=0;
-	int right=0;
-	int i=0;
-	int j=0;
-	while(a[i]<x) i++;
-	while(a[j]>x) j--;
+	int i=left;
+	int j=right;
+	while(i<=j){
+		while(a[i]<x) i++;
+		while(a[j]>x) j--;
+		if(i<=j){
+			int t=a[i];
+			a[i]=a[j];
+			a[j]=t;
+			i++;
+			j--;
+		}
+	}
+	if(left<j) quickSort(left,j,a);
+	if(i<right) quickSort(i,right,a);
 }
 int main(){
 	int n;
 	cin>>n;
-	vector<int> a;
+	int a[n+1];
 	for(int i=1;i<=n;i++){
-		int k;
-		cin>>k;
-		a.push_back(k);
+		cin>>a[i];
 	}
-	quickSort(a);
+	quickSort(1,n,a);
+	for(int i=1;i<=n;i++){
+		cout<<a[i]<<" ";
+	}
 }
